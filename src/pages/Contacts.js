4e8122b62561df4b53e8c3a76cc20c15d5a8dea8
Almazbi <frontend477@gmail.com>
2023-09-21 20:19:23 +0600

    first

---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+    return (
+		<main className="section">
+			<div className="container">
+				<h1 className="title-1">Contacts</h1>
+
+				<ul className="content-list">
+					<li className="content-list__item">
+						<h2 className="title-2">Location</h2>
+						<p>Bishkek, Kara-Balta</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Telegram / WhatsApp</h2>
+						<p>
+							<a href="tel:+996220139064">+996 (220) 13 90 64</a>
+						</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Email</h2>
+						<p>
+							<a href="mailto:frontend477@gmail.com">
+							frontend477@gmail.com
+							</a>
+						</p>
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
+}
+
+export default Contacts;
